d5466edf72c1491b29056ecc9bdbadd0570a036a
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2022-01-26 16:48:33 +0530

    first commit

--- data/database.js
+++ data/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  database: "blog",
-  user: "root",
-  password: "0258",
+  host: "us-cdbr-east-05.cleardb.net",
+  database: "heroku_dc7870cc5f71c3c",
+  user: "b03eb642afd46b",
+  password: "a7f28eca",
 });
 
 module.exports = pool;
