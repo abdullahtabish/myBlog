4ec8867343aec55b6972f37945f6590a0dbed618
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2024-03-10 02:35:09 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose'); // Import mongoose module for MongoDB connection
+const mongoose = require('mongoose');
 
 // MongoDB connection URI
 const mongoURI = 'mongodb+srv://count:0258@testing.7u8zleq.mongodb.net/?retryWrites=true&w=majority';
@@ -8,7 +8,23 @@ mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('Connected to MongoDB')) // Log connection success
-  .catch(err => console.error('Error connecting to MongoDB:', err)); // Log connection error
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
-module.exports = mongoose; // Export mongoose for use in other files
+const postSchema = new mongoose.Schema({
+  title: String,
+  summary: String,
+  body: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+module.exports = { Post, Author };
