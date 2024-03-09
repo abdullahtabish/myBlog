d0e5049072c58404c57e32cc0563d7931feabf64
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2024-03-10 02:38:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const path = require("path");
-
 const express = require("express");
-
 const blogRoutes = require("./routes/blog");
-
 const mongoose = require("mongoose");
 
 const app = express();
@@ -16,15 +13,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.static("public")); // Serve static files (e.g. CSS files)
 
+// Define route handler for the root URL
+app.get("/", (req, res) => {
+  res.redirect("/posts"); // Redirect to /posts
+});
+
 app.use(blogRoutes);
 
 app.use(function (error, req, res, next) {
   // Default error handling function
   // Will become active whenever any route / middleware crashes
-  console.log(error);
+  console.error(error);
   res.status(500).render("500");
 });
 
 app.listen(port, () => {
-  console.log(`listening to the port at ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
