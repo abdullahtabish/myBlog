765d3dcc87f49a42f4db87897c0a8b3ee7892def
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2024-03-10 02:19:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const blogRoutes = require("./routes/blog");
 
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
